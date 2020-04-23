e062fc94fb2e600ad42afe5cb3d41c226ca8c066
Alexander Chiglintsev <alex.chiglintsev@yandex.ru>
2020-04-23 15:55:32 +0800

    Add CRUD-methods bodies

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,70 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = size() - 1; i >= 0; i--) {
+            if (storage[i] != null) {
+                storage[i] = null;
+            } else break;
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i <= size(); i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i <= size() - 1; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int numdel = 0;
+        for (int i = 0; i <= size() - 1; i++) {
+            numdel++;
+            if (storage[i].uuid.equals(uuid)) {
+                break;
+            }
+        }
+        storage[numdel - 1] = storage[size() - 1];
+        storage[size() - 1] = null;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] allres = new Resume[size()];
+        int k = 0;
+        for (int i = 0; i <= size() - 1; i++) {
+            if (storage[i] != null) {
+                allres[k] = storage[i];
+                k++;
+            } else {
+                break;
+            }
+        }
+        return allres;
     }
 
     int size() {
-        return 0;
+        int i = 0;
+        for (Resume res : storage) {
+            if (res != null) {
+                i++;
+            }
+        }
+        return i;
     }
 }
