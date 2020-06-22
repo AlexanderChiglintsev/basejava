2633443969ed0ff91cb5aedb0951770539553dcf
Alexander Chiglintsev <alex.chiglintsev@yandex.ru>
2020-06-22 20:59:01 +0300

    Add abstract class AbstractArrayStorage

---
+++ src/ru/snx/webapp/storage/AbstractArrayStorage.java
@@ -0,0 +1,44 @@
+package ru.snx.webapp.storage;
+
+import ru.snx.webapp.model.Resume;
+
+import java.util.Arrays;
+
+public abstract class AbstractArrayStorage implements Storage {
+    protected static final int STORAGE_CAPACITY = 100000;
+    protected Resume[] storage = new Resume[STORAGE_CAPACITY];
+    protected int size = 0;
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void update(Resume r) {
+        int index = findIndex(r.getUuid());
+        if (index != -1) {
+            storage[index] = r;
+        } else {
+            System.out.println("Резюме " + r.getUuid() + " отсутствует !!!");
+        }
+    }
+
+    public Resume get(String uuid) {
+        int index = findIndex(uuid);
+        if (index != -1) {
+            return storage[index];
+        }
+        System.out.println("Резюме " + uuid + " отсутствует !!!");
+        return null;
+    }
+
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    protected abstract int findIndex(String uuid);
+}
--- src/ru/snx/webapp/storage/ArrayStorage.java
+++ src/ru/snx/webapp/storage/ArrayStorage.java
@@ -2,29 +2,11 @@ package ru.snx.webapp.storage;
 
 import ru.snx.webapp.model.Resume;
 
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage implements Storage {
-    private static final int STORAGE_CAPACITY = 10000;
-    private Resume[] storage = new Resume[STORAGE_CAPACITY];
-    private int size = 0;
+public class ArrayStorage extends AbstractArrayStorage {
 
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
-
-    public void update(Resume r) {
-        int index = findIndex(r.getUuid());
-        if (index != -1) {
-            storage[index] = r;
-        } else {
-            System.out.println("Резюме " + r.getUuid() + " отсутствует !!!");
-        }
-    }
 
     public void save(Resume r) {
         if (size < STORAGE_CAPACITY) {
@@ -38,15 +20,6 @@ public class ArrayStorage implements Storage {
         } else {
             System.out.println("База резюме переполнена !!!");
         }
-    }
-
-    public Resume get(String uuid) {
-        int index = findIndex(uuid);
-        if (index != -1) {
-            return storage[index];
-        }
-        System.out.println("Резюме " + uuid + " отсутствует !!!");
-        return null;
     }
 
     public void delete(String uuid) {
@@ -63,18 +36,12 @@ public class ArrayStorage implements Storage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
-    }
-
-    public int size() {
-        return size;
-    }
 
     /*Поиск резюме по uuid
     Возвращает индекс элемента в storage, если найдено,
     в противном случае -1*/
-    private int findIndex(String uuid) {
+    @Override
+    protected int findIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 return i;
