b37e25049a4365d9fead5bdfcc2926129a9aabae
Alexander Chiglintsev <alex.chiglintsev@yandex.ru>
2020-08-04 19:07:29 +0300

    Add logging

--- src/ru/snx/webapp/storage/AbstractStorage.java
+++ src/ru/snx/webapp/storage/AbstractStorage.java
@@ -6,33 +6,40 @@ import ru.snx.webapp.model.Resume;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<SK> implements Storage {
 
     private static final Comparator<Resume> RESUME_COMPARATOR = Comparator.comparing(Resume::getFullName).thenComparing(Resume::getUuid);
+    private final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
 
     @Override
     public void update(Resume r) {
+        LOG.info("Update: " + r);
         updateResume(checkUuidNoExist(r.getUuid()), r);
     }
 
     @Override
     public void save(Resume r) {
+        //LOG.info("Save: " + r);
         insertResume(checkUuidExist(r.getUuid()), r);
     }
 
     @Override
     public Resume get(String uuid) {
+        LOG.info("Get: " + uuid);
         return getResume(checkUuidNoExist(uuid));
     }
 
     @Override
     public void delete(String uuid) {
+        LOG.info("Delete: " + uuid);
         deleteResume(checkUuidNoExist(uuid));
     }
 
     @Override
     public List<Resume> getAllSorted() {
+        LOG.info("getAllSorted");
         List<Resume> sortedResumes = getAllSortedResume();
         sortedResumes.sort(RESUME_COMPARATOR);
         return sortedResumes;
@@ -41,6 +48,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     private SK checkUuidExist(String uuid) {
         SK key = findKey(uuid);
         if (checkExist(key)) {
+            LOG.warning("Резюме " + uuid + " уже существует !!!");
             throw new ExistStorageException(uuid);
         }
         return key;
@@ -49,6 +57,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     private SK checkUuidNoExist(String uuid) {
         SK key = findKey(uuid);
         if (!checkExist(key)) {
+            LOG.warning("Резюме " + uuid + " отсутствует !!!");
             throw new NoExistStorageException(uuid);
         }
         return key;
