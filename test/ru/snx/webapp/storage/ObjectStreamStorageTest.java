3e962c1bf8e503db9b1083e82460311e4eda821d
Alexander Chiglintsev <alex.chiglintsev@yandex.ru>
2020-10-01 17:10:15 +0300

    HW9: Add ObjectStreamSerializer implements Serializer

--- test/ru/snx/webapp/storage/ObjectStreamStorageTest.java
+++
@@ -1,7 +0,0 @@
-package ru.snx.webapp.storage;
-
-public class ObjectStreamStorageTest extends AbstractStorageTest {
-    public ObjectStreamStorageTest() {
-        super(new ObjectStreamStorage(STORAGE_DIR));
-    }
-}
